905c14105dde0806fffc103dcb833442cee30a3a
Eminion14 <eramirezc145@gmail.com>
2023-07-07 20:06:33 -0600

    07/07/2023

--- app.ts
+++ app.ts
@@ -1,14 +1,1 @@
-function openPopupWindow(url: string, width: number, height: number) {
-    const left = (window.screen.width - width) / 2;
-    const top = (window.screen.height - height) / 2;
-  
-    const options = `width=${width},height=${height},left=${left},top=${top}`;
-    window.open(url, '_blank', options);
-  }
-  
-  // Example usage
-  const popupUrl = 'https://github.com';
-  const popupWidth = 500;
-  const popupHeight = 400;
-  
-  openPopupWindow(popupUrl, popupWidth, popupHeight);
+console.log("Hi mom!");
