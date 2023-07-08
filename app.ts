c483bec9beeaee244e81b62338fa05f944b0a163
Eminion14 <eramirezc145@gmail.com>
2023-07-07 19:17:18 -0600

    added style tag to center and popup window in TS file

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Hola mamá');
+function openPopupWindow(url: string, width: number, height: number) {
+    const left = (window.screen.width - width) / 2;
+    const top = (window.screen.height - height) / 2;
+  
+    const options = `width=${width},height=${height},left=${left},top=${top}`;
+    window.open(url, '_blank', options);
+  }
+  
+  // Example usage
+  const popupUrl = 'https://github.com';
+  const popupWidth = 500;
+  const popupHeight = 400;
+  
+  openPopupWindow(popupUrl, popupWidth, popupHeight);
